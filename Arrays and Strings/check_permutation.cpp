bf50bcd51e3257e687b1e7fc3fafd37a30b2a19c
Michael Ganzorig <manlai1025@gmail.com>
2020-05-26 21:46:09 -0400

    solution to check permutation

---
+++ Arrays and Strings/check_permutation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <map>
+
+/*
+ * NOTE: compile with C++11
+ * execute the below command to run:
+ * c++ -std=c++11 Arrays\ and\ Strings/check_permutation.cpp && ./a.out
+ */
+bool check_permutation(std::string s1, std::string s2)
+{
+    // frequency of letters in s1
+    std::map<char, int> freq;
+    
+    // counting how many from each letter s1 has
+    for(int i = 0; i < s1.size(); i++)
+        freq[s1[i]] = freq.find(s1[i]) == freq.end() ? 1 : freq[s1[i]] + 1;
+
+    // checking off which s1's letter does s2 also contain
+    // we do -1 because a letter can occur more than once
+    for(int i = 0; i < s2.size(); i++)
+        if(freq.find(s2[i]) != freq.end() && freq[s2[i]] > 0)
+            freq[s2[i]] -= 1;
+        else
+            return false;
+
+    // edge case where s1 has extra letters where s2 doesn't have
+    for(std::pair<char, int> p : freq)
+        if(p.second != 0)
+            return false;
+    return true;
+}
+
+int main()
+{
+    // 1 is true, 0 is false
+    std::cout << check_permutation("hi", "ih") << std::endl;
+    std::cout << check_permutation("hello", "ehlol") << std::endl;
+    std::cout << check_permutation("hello", "ehlel") << std::endl;
+    std::cout << check_permutation("world", "") << std::endl;
+}
