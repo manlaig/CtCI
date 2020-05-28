1e3caaca81f5fc6dccbf7b6d611c46946930ae9b
Michael Ganzorig <manlai1025@gmail.com>
2020-05-28 03:57:10 -0400

    solution to no buffer remove_dups

--- Linked Lists/remove_dups.cpp
+++ Linked Lists/remove_dups.cpp
@@ -25,6 +25,30 @@ void remove_dups(Node*& head)
     }
 }
 
+void remove_dups_no_buffer(Node*& head)
+{
+    Node* out = nullptr;
+    Node* curr = head;
+    while(curr)
+    {
+        bool found = false;
+        Node* trav = out;
+        while(trav)
+        {
+            if(trav->val == curr->val)
+            {
+                found = true;
+                break;
+            }
+            trav = trav->next;
+        }
+        if(!found)
+            LL::addToTail(out, curr->val);
+        curr = curr->next;
+    }
+    head = out;
+}
+
 int main()
 {
     using namespace LL;
@@ -41,9 +65,24 @@ int main()
     addToHead(head, 12);
     std::cout << "before: ";
     print(head);
-    std::cout << "after: ";
+    std::cout << "remove_dups: ";
     remove_dups(head);
     print(head);
+
+    Node* head4 = nullptr;
+    addToTail(head4, 10);
+    addToTail(head4, 15);
+    addToTail(head4, 10);
+    addToTail(head4, 20);
+    addToTail(head4, 20);
+    addToTail(head4, 20);
+    addToHead(head4, 11);
+    addToHead(head4, 12);
+    addToHead(head4, 12);
+    addToHead(head4, 12);
+    std::cout << "remove_dups_no_buffer: ";
+    remove_dups(head4);
+    print(head4);
     std::cout << std::endl;
 
     Node* head2 = nullptr;
@@ -52,15 +91,28 @@ int main()
     addToTail(head2, 20);
     std::cout << "before: ";
     print(head2);
-    std::cout << "after: ";
+    std::cout << "remove_dups: ";
     remove_dups(head2);
     print(head2);
+
+    Node* head5 = nullptr;
+    addToTail(head5, 20);
+    addToTail(head5, 20);
+    addToTail(head5, 20);
+    std::cout << "remove_dups_no_buffer: ";
+    remove_dups(head5);
+    print(head5);
     std::cout << std::endl;
 
     Node* head3 = 0;
     std::cout << "before: ";
     print(head3);
-    std::cout << "after: ";
+    std::cout << "remove_dups: ";
     remove_dups(head3);
     print(head3);
+
+    Node* head6 = 0;
+    std::cout << "remove_dups_no_buffer: ";
+    remove_dups(head6);
+    print(head6);
 }
