43317f4b3ff4ea24dca70a762c72bc9ffd6c4b93
Michael Ganzorig <manlai1025@gmail.com>
2020-05-28 17:12:16 -0400

    solution to partition

---
+++ Linked Lists/partition.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "Node.h"
+
+// partition the Linked list around the value x
+void partition(Node*& head, int x)
+{
+    Node *left = 0, *right = 0;
+
+    while(head)
+    {
+        // save this pointer since we'll override it below
+        Node* temp = head->next;
+        if(head->val < x)
+        {
+            head->next = left;
+            left = head;
+        }
+        else
+        {
+            head->next = right;
+            right = head;
+        }
+        head = temp;
+    }
+    // connecting left and right together
+    Node** l = &left;
+    while(*l)
+        l = &((*l)->next);
+    *l = right;
+    head = left;
+}
+
+int main()
+{
+    using namespace LL;
+    Node* head = 0;
+    addToTail(head, 1);
+    addToTail(head, 5);
+    addToTail(head, 4);
+    addToTail(head, 3);
+    addToTail(head, 2);
+    addToTail(head, 6);
+    print(head);
+    partition(head, 3);
+    print(head);
+}
