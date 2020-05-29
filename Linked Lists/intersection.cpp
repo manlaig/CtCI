ee3e83fa093e54753ed4be00d65f4aa53fd26d9b
Michael Ganzorig <manlai1025@gmail.com>
2020-05-29 00:19:33 -0400

    fast intersection finding solution

--- Linked Lists/intersection.cpp
+++ Linked Lists/intersection.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
+#include <set>
 #include "Node.h"
 
 // This is a kind of Brute Force way of doing
 Node* intersection(Node* l1, Node* l2)
 {
-    // 0->1->2->3
-    // 1->1->1->2->3
-
     while(l1)
     {
         Node* temp = l2;
@@ -17,6 +15,23 @@ Node* intersection(Node* l1, Node* l2)
             temp = temp->next;
         }
         l1 = l1->next;
+    }
+    return nullptr;
+}
+
+Node* fast_intersection(Node* l1, Node* l2)
+{
+    std::set<Node*> visited;
+    while(l1)
+    {
+        visited.insert(l1);
+        l1 = l1->next;
+    }
+    while(l2)
+    {
+        if(visited.find(l2) != visited.end())
+            return l2;
+        l2 = l2->next;
     }
     return nullptr;
 }
@@ -35,5 +50,7 @@ int main()
     LL::print(l2);
 
     Node* inter = intersection(l1, l2);
-    std::cout << (inter ? inter->val : -1) << std::endl;
+    std::cout << "Brute Force: " << (inter ? inter->val : -1) << std::endl;
+    Node* fast = fast_intersection(l1, l2);
+    std::cout << "Fast Intersection: " << (fast ? fast->val : -1) << std::endl;
 }
