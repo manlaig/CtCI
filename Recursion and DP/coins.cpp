543de2f6dbbf11c1fa89ef7522e5d56567c8d047
Michael Ganzorig <manlai1025@gmail.com>
2020-07-11 15:27:16 -0400

    solution

--- Recursion and DP/coins.cpp
+++ Recursion and DP/coins.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
 
-int coins(int n)
+int coins_forward(int n)
 {
     std::vector<int> coins = {1, 5, 10, 25};
-    std::vector<int> dp(n+1);
+    std::vector<int> dp(n+1, 0);
     dp[0] = 0;
     for(int i : coins)
         dp[i] = 1;
@@ -20,9 +20,25 @@ int coins(int n)
     return dp[n];
 }
 
+int coins_back(int n)
+{
+    std::vector<int> coins = {1, 5, 10, 25};
+    std::vector<int> dp(n+1, 0);
+    dp[0] = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int coin : coins)
+            if(i - coin >= 0)
+                dp[i] += dp[i-coin];
+    }
+    return dp[n];
+}
+
 int main()
 {
-    std::cout << coins(6) << std::endl;     // [1,5], [5,1], [1,1,1,1,1,1]
-    std::cout << coins(7) << std::endl;     // [1,1,5], [5,1,1], [1,5,1], [1,1,1,1,1,1,1]
-    std::cout << coins(10) << std::endl;
+    std::cout << coins_forward(6) << " " << coins_back(6) << std::endl;     // [1,5], [5,1], [1,1,1,1,1,1]
+    std::cout << coins_forward(7) << " " << coins_back(7) << std::endl;     // [1,1,5], [5,1,1], [1,5,1], [1,1,1,1,1,1,1]
+    std::cout << coins_forward(10) << " " << coins_back(10) << std::endl;
+    std::cout << coins_forward(25) << " " << coins_back(25) << std::endl;
 }
