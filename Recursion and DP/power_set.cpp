953c72cc8a7300ac699f8475205bc072cd543b7b
Michael Ganzorig <manlai1025@gmail.com>
2020-07-11 11:40:58 -0400

    iterative solution

--- Recursion and DP/power_set.cpp
+++ Recursion and DP/power_set.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 using namespace std;
 
+// Solution 1
 // This solution is O(2^n), n is the size of elements in the array
 // The power set consists of 2^n number of elements
 void power_set(vector<vector<int>>& out, const vector<int>& arr, vector<int> buf, int curr=0)
@@ -26,14 +27,38 @@ vector<vector<int>> power_set(const vector<int>& arr)
     return set;
 }
 
+// Solution 2
+vector<vector<int>> power_set_iter(const vector<int>& arr)
+{
+    vector<vector<int>> out;
+    out.reserve(pow(2, arr.size()));
+    for(int i = 0; i < pow(2, arr.size()); i++)
+    {
+        int count = i;
+        vector<int> subset;
+        for(int num : arr)
+        {
+            if(!count)
+                break;
+            if(count % 2)
+                subset.push_back(num);
+            count /= 2;
+        }
+        out.push_back(subset);
+    }
+    return out;
+}
+
 void print(const vector<vector<int>>& arr)
 {
     for(int i = 0; i < arr.size(); i++)
     {
+        cout << "[";
         for(int j = 0; j < arr[i].size(); j++)
-            cout << arr[i][j] << " ";
-        cout << endl;
+            cout << arr[i][j] << (j != arr[i].size()-1 ? ", " : "");
+        cout << (i != arr.size()-1 ? "], " : "");
     }
+    cout << "]" << endl;
 }
 
 int main()
@@ -41,11 +66,15 @@ int main()
     {
         // including the empty set, the size is 8
         auto set = power_set({1,2,3});
+        auto set2 = power_set_iter({1,2,3});
         print(set);
+        print(set2);
     }
     {
         // including the empty set, the size is 16
-        auto set = power_set({1,2,3,4});
+        auto set = power_set_iter({1,2,3,4});
+        auto set2 = power_set_iter({1,2,3,4});
         print(set);
+        print(set2);
     }
 }
